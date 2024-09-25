e0fa291455a64b92f74a37b71ad9f3969e112f9b
rtof06 <ricardoofilho06@gmail.com>
2024-09-25 20:45:46 -0300

    top

--- aula13/src/components/Rodape/Rodape.tsx
+++ aula13/src/components/Rodape/Rodape.tsx
@@ -3,7 +3,7 @@ export default function Rodape() {
   return (
     <footer>
         <h2>Rodape</h2>
-        <p>Desenvolvido por <span>pf765</span></p>
+        <p>Desenvolvido por <span>rtof06</span></p>
     </footer>
   )
 }
