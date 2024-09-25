98ae4c915020bcdded9d0237d3bc38f7c7e5bc9d
davignz <werdavid1@gmail.com>
2024-09-25 20:48:17 -0300

    router handlers com rotas alinhadas e dinamicas

--- aula13/src/app/produtos/page.tsx
+++ aula13/src/app/produtos/page.tsx
@@ -1,48 +1,48 @@
-import Link from "next/link"
+import Link from "next/link";
 
 export default function Produtos() {
-    const produtos = [
-        {id: 1, nome: "Mouse", preco: 100.0, qtd: 3 },
-        {id: 2, nome: "Monitor", preco: 760.5, qtd: 1 },
-        {id: 3, nome: "Headset", preco: 250.0, qtd: 2 },
-        {id: 4, nome: "Teclado", preco: 1.0, qtd: 1 },
-        {id: 5, nome: "Cabo LAN", preco: 5.0, qtd: 5 }
-    ]
+  const produtos = [
+    { id: 1, nome: "Mouse", preco: 100.0, qtd: 3 },
+    { id: 2, nome: "Monitor", preco: 760.5, qtd: 1 },
+    { id: 3, nome: "Headset", preco: 250.0, qtd: 2 },
+    { id: 4, nome: "Teclado", preco: 1.0, qtd: 1 },
+    { id: 5, nome: "Cabo LAN", preco: 5.0, qtd: 5 },
+  ];
 
-    return (
-        <div>
-            <h2>Produtos</h2>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Id</th>
-                        <th>Nome</th>
-                        <th>Preço</th>
-                        <th>Quantidade</th>
-                        <th>Editar</th>
-                    </tr>
-                </thead>
+  return (
+    <div>
+      <h2>Produtos</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Nome</th>
+            <th>Preço</th>
+            <th>Quantidade</th>
+            <th>Editar</th>
+          </tr>
+        </thead>
 
-                <tbody>
-                    {produtos.map((p) => (
-                        <tr key={p.id}>
-                            <td>{p.id}</td>
-                            <td>{p.nome}</td>
-                            <td>{p.preco}</td>
-                            <td>{p.qtd}</td>
-                            <td><Link href={`/produto/${p.id}`}>Editar</Link></td>
-                        </tr>
-                    ))}
-                </tbody>
+        <tbody>
+          {produtos.map((p) => (
+            <tr key={p.id}>
+              <td>{p.id}</td>
+              <td>{p.nome}</td>
+              <td>{p.preco}</td>
+              <td>{p.qtd}</td>
+              <td>
+                <Link href={`/produtos/produto/${p.id}`}>Editar</Link>
+              </td>
+            </tr>
+          ))}
+        </tbody>
 
-                <tfoot>
-                    <tr>
-                        <th>
-
-                        </th>
-                    </tr>
-                </tfoot>
-            </table>
-        </div>
-    )
+        <tfoot>
+          <tr>
+            <th></th>
+          </tr>
+        </tfoot>
+      </table>
+    </div>
+  );
 }
