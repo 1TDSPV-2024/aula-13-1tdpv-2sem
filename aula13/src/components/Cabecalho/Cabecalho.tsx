c77cda8de6063145629c25e2b17520a728f15686
pyz1m <samuelyariwake@gmail.com>
2024-09-27 21:22:34 -0300

    feito

--- aula13/src/components/Cabecalho/Cabecalho.tsx
+++ aula13/src/components/Cabecalho/Cabecalho.tsx
@@ -3,7 +3,7 @@ import Menu from "../Menu/Menu";
 export default function Cabecalho() {
   return (
     <header className="cabecalho">
-        <h1>Cabecalho</h1>
+        <h1>testao</h1>
         <Menu/>
     </header>
   )
